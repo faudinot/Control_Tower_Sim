d0199a27ce6b33828fc4dd83726e04836a3ed222
faudinot <f.audinot@gmail.com>
2021-03-30 09:17:00 +0200

    Declare functions for PlaneCommercial

--- PlaneCommercial.cpp
+++ PlaneCommercial.cpp
@@ -1,11 +1,27 @@
 #include "PlaneCommercial.h"
 
-PlaneCommercial::PlaneCommercial(const std::string& name) : PlaneAbstract(name)
-{
+#include <sstream>
 
+PlaneCommercial::PlaneCommercial(const std::string& name) : PlaneAbstract(), _type(Type_Plane::COMMERCIAL)
+{
+    RandomGeneratorInt random_number{1, 100};
+    _passengers_count = random_number();
+    _name = name;
 }
 
 PlaneCommercial::~PlaneCommercial()
 {
+}
 
+std::string PlaneCommercial::getInfo()
+{
+    std::stringstream sstring;
+    sstring << _name << "\t" << "Passengers : " << _passengers_count;
+
+    return sstring.str();
+}
+
+std::chrono::milliseconds PlaneCommercial::getTimeToWait()
+{
+    return std::chrono::milliseconds(_passengers_count * TIME_PER_PASSENGER);
 }
--- PlaneCommercial.h
+++ PlaneCommercial.h
@@ -1,16 +1,22 @@
 #ifndef PLANECOMMERCIAL_H
 #define PLANECOMMERCIAL_H
 
-#include <PlaneAbstract.h>
+#include "PlaneAbstract.h"
+#include "utils.h"
 
-class PlaneCommercial : PlaneAbstract
+class PlaneCommercial : public PlaneAbstract
 {
 public:
     PlaneCommercial(const std::string& name);
     ~PlaneCommercial();
 
+    std::string getInfo();
+
+    std::chrono::milliseconds getTimeToWait();
+
 private:
     unsigned int _passengers_count;
+    Type_Plane _type;
 };
 
 #endif // PLANECOMMERCIAL_H
